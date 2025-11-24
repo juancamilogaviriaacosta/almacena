2f8da59cda53fc3bb61170a2a9274fa32d76512d
Juan Gaviria <juancamilogaviriaacosta@gmail.com>
2025-11-24 16:46:26 -0500

    Commit juan 1764020786

---
+++ almacenaws/src/main/java/com/almacenaws/controller/AngularController.java
@@ -0,0 +1,13 @@
+package com.almacenaws.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AngularController {
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
