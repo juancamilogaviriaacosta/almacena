941726d64e9f0bea97ae2faef23620952ebaaa85
Juan Gaviria <juancamilogaviriaacosta@gmail.com>
2026-03-14 02:17:37 -0500

    Commit juan 1773472657

--- almacena-fe/src/app/app.routes.ts
+++ almacena-fe/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { Home } from './home/home';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
+    { path: '', component: Home },
 ];
