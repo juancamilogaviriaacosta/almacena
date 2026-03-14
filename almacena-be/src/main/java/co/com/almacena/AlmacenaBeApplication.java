809478938c184a90cde8d496a1b08fbd92cace63
Juan Gaviria <juancamilogaviriaacosta@gmail.com>
2026-03-13 22:57:56 -0500

    Commit juan 1773460676

---
+++ almacena-be/src/main/java/co/com/almacena/AlmacenaBeApplication.java
@@ -0,0 +1,13 @@
+package co.com.almacena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacenaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmacenaBeApplication.class, args);
+	}
+
+}
